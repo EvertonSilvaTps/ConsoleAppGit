a6840d6deab22a0b0067ac1405b8989714e72e7f
EvertonSilvaTps <everton.tps@outlook.com>
2025-12-07 18:41:31 -0300

    add> adicionado o dono do carro

---
+++ ConsoleAppGit/Dono.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppGit
+{
+    public class Dono
+    {
+        public string Nome {  get; private set; }
+    }
+}
