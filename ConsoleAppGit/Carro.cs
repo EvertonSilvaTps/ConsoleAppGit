27ca46cba39900718b6d533885305f93b2f3bb37
EvertonSilvaTps <everton.tps@outlook.com>
2025-12-07 18:01:13 -0300

    Add: atributos do carro

--- ConsoleAppGit/Carro.cs
+++ ConsoleAppGit/Carro.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace ConsoleAppGit
 {
-    internal class carro
+    internal class Carro
     {
+        public string Placa { get; set; }
+        public string Cor {  get; set; }
+        public int Potencia { get; set; }
+        public bool Flex { get; set; }
     }
 }
